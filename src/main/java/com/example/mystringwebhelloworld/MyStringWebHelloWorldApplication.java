c20b1e50f3657cafdbe79175283bb624bcb04835
rzhadkpi <zhadko.ruslan@lll.kpi.ua>
2024-09-27 22:06:26 +0300

    Init commit

---
+++ src/main/java/com/example/mystringwebhelloworld/MyStringWebHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.mystringwebhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStringWebHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStringWebHelloWorldApplication.class, args);
+    }
+
+}
